49b1c13399c844dbeac4e468bfd6b7824fc64e03
Deanmark <deansy0718@gmail.com>
2026-01-11 17:26:52 +0800

    arrays and loops part 1 DONE

--- 11-arrays-and-loops/exercises/11a.js
+++ 11-arrays-and-loops/exercises/11a.js
@@ -22,3 +22,99 @@ function arraySwap(array) {
 }
 console.log(arraySwap([1, 2, 3]));
 //11d
+for (let i = 0; i <= 10; i+=2) { //count 0-10 but 2,4,6...
+    console.log(i)
+}
+//11e
+for (let i = 5; i > 0; i--) {
+    console.log(i) //5-1
+}
+//11f
+let i = 0;
+while (i < 10) {
+    i += 2;
+    console.log(i)
+}
+//11g
+const myArray = [1, 2, 3];
+let total = [];
+for (let i = 0; i < myArray.length; i++) {
+    total.push(myArray[i] + 1)
+}
+console.log(total); // double the array value to 1
+//11h
+function addOne(array) {
+    let total = [];
+    for (let i = 0; i <array.length; i++) {
+        total.push(array[i] + 1)
+    }
+    return total;
+}
+console.log(addOne([1,2,3]));
+//11i
+function addNum(array, num) {
+    let total = [];
+    for (let i = 0; i < array.length; i++) {
+        total.push(array[i] + num);
+    }
+return total;
+}
+console.log(addNum([1,2,3], 2));
+//11j
+function addArray(array1, array2) {
+    let total = [];
+    for (let i = 0; i < array1.length; i++) {
+        total.push(array1[i] + array2[i])
+    }
+    return total;
+}
+console.log(addArray([1,1,2], [1,1,3]));
+//11k
+function countPositive(nums) {
+    let positive = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            positive += 1;
+        }
+    }
+    return positive;
+}
+console.log(countPositive([1, -3, 5]));
+//11l
+function minMax(nums) {
+    let totals = {
+        min: nums[0],
+        max: nums[0]
+    };
+    
+    for (let i = 0; i < nums.length; i++) {
+        let value = nums[i];
+
+        if (totals.min === null || value < totals.min) {
+            totals.min = value;
+        }
+
+        if (totals.max === null || value > totals.max) {
+            totals.max = value;
+        }
+    }
+    return totals;
+}
+console.log(minMax([1, -3, 5]))
+console.log(minMax([2, 3]))
+//11n 
+function countWords(words) {
+    let appeared = {};
+        for (let i = 0; i < words.length; i++) {
+            let fruits = words[i];
+
+            if (!appeared[fruits]) {
+                appeared[fruits] = 1;
+            }
+            else {
+                appeared[fruits]++;
+            }
+        }  
+    return appeared;
+}
+console.log(countWords(['apple', 'grape', 'apple', 'apple']))
