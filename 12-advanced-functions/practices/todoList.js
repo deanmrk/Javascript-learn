77014564cc3cd054c1232afd0035dc6ac4b98b82
Deanmark <deansy0718@gmail.com>
2026-01-13 20:52:46 +0800

    changed the loop method to forEach method

---
+++ 12-advanced-functions/practices/todoList.js
@@ -0,0 +1,48 @@
+const myArray = [
+    {name: 'Wash', date: '2025-09-01'},
+    {name: 'Bath', date: '2025-09-10'}
+];
+renderData()
+function renderData() {
+    const container = document.querySelector('.container');
+    let html = '';
+
+    myArray.forEach(function (getValObject, i) {
+        const {name, date} = getValObject;
+
+        html += `
+                <div>${name}</div>
+                <div>${date}</div>
+                <button class="js-delete"
+                 onclick="myArray.splice(${i}, 1)
+                    renderData(); ">Delete
+                </button>
+                `
+    });
+    container.innerHTML = html;
+    return html;
+}
+
+function text() {
+    const input = document.querySelector('.js-input');
+    const date = document.querySelector('.js-date');
+    
+    getInput = input.value;
+    getDate = date.value;
+
+    if (!getInput || !getDate) {
+        alert('Please fill all the blanks!');
+    }
+    else {
+    myArray.push({name: getInput, date: getDate}); //can also use shorthand property if the name of two properties is same
+    renderData()
+    }
+    input.value = '';
+    date.value = '';
+};
+
+function enter(event) {
+    if (event.key === 'Enter') {
+        text();
+    }
+}
